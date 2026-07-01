bab5623f9856c9398a6d4f430fbf90c8ed9c9b55
Abubakar <142691379+Abubakar230@users.noreply.github.com>
2026-07-01 06:43:44 +0500

    Directory Loader

---
+++ (7_LangChain_DocumentLoader)/03_directoryLoader.py
@@ -0,0 +1,14 @@
+# fetch all pdf from the directory/folder and load them using PyPDFLoader
+
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+
+loader = DirectoryLoader(
+    path='books',
+    glob='*.pdf',
+    loader_cls=PyPDFLoader
+)
+
+docs = loader.lazy_load()
+
+for document in docs:
+    print(document.metadata)
