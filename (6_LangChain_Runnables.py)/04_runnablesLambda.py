27a4b50a700b53095469ed42eaf27dbccbe3ffa3
Abubakar <142691379+Abubakar230@users.noreply.github.com>
2026-07-01 05:46:21 +0500

    Runnables Branch and Lambda

---
+++ (6_LangChain_Runnables.py)/04_runnablesLambda.py
@@ -0,0 +1,44 @@
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
+
+load_dotenv()
+
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    # repo_id="meta-llama/Llama-3.3-70B-Instruct",
+    # repo_id="meta-llama/Llama-3.1-70B-Instruct",
+    task="text-generation",
+    # timeout=300,      # 5 minutes
+    # max_new_tokens=100,
+)
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'word_count': RunnableLambda(word_count)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
---
+++ (6_LangChain_Runnables.py)/05_runnablesBranch.py
@@ -0,0 +1,43 @@
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    # repo_id="meta-llama/Llama-3.3-70B-Instruct",
+    # repo_id="meta-llama/Llama-3.1-70B-Instruct",
+    task="text-generation",
+    # timeout=300,      # 5 minutes
+    # max_new_tokens=100,
+)
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+report_gen_chain = prompt1 | model | parser
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>300, prompt2 | model | parser),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
+
+
