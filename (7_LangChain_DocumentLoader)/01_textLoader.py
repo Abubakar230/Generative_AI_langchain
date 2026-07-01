5b1a3cf6e27730b743e4ecebba2c13007b713d7f
Abubakar <142691379+Abubakar230@users.noreply.github.com>
2026-07-01 06:27:58 +0500

    TextLoader

---
+++ (7_LangChain_DocumentLoader)/01_textLoader.py
@@ -0,0 +1,41 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    # repo_id="meta-llama/Llama-3.3-70B-Instruct",
+    # repo_id="meta-llama/Llama-3.1-70B-Instruct",
+    task="text-generation",
+    # timeout=300,      # 5 minutes
+    # max_new_tokens=100,
+)
+model = ChatHuggingFace(llm=llm)
+
+
+prompt = PromptTemplate(
+    template='Write a summary for the following poem - \n {poem}',
+    input_variables=['poem']
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader('cricket.txt', encoding='utf-8')
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs[0].page_content}))
